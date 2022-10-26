58e2d3476edd0d30ccfb7a1b852f8c0b3ca90470
majharul224 <majharulislam2225@gmail.com>
2022-10-26 23:46:50 +0600

    edite a login form

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -12,11 +12,11 @@ const Login = () => {
     const form = event.target
     const email = form.email.value;
     const password = form.password.value;
-    console.log(email,password)
+    console.log(email, password)
     signin(email, password)
       .then(result => {
         const user = result.user
-        form.reset() 
+        form.reset()
         naviget('/')
         console.log(user)
       })
@@ -25,26 +25,22 @@ const Login = () => {
       })
   }
   return (
-    <Form onSubmit={handleSubmit}>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" name='email' placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
+    <div className='w-50 mx-auto'>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" name='email' placeholder="Enter email" />
+        </Form.Group>
 
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" name="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" name="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 };
 
