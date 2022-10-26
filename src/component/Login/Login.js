1b144cd7a62b2b5615cd01521ae24115f3def9d1
majharul224 <majharulislam2225@gmail.com>
2022-10-26 23:38:30 +0600

    added a log in component

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,51 +1,51 @@
-// import React, { useContext } from 'react';
-// import { Button, Form } from 'react-bootstrap';
-// import { useNavigate } from 'react-router-dom';
-// import { AuthContext } from '../../context/UserContext';
+import React, { useContext } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/UserContext';
 
 
-// const Login = () => {
-//   const { signin } = useContext(AuthContext)
-//   const naviget = useNavigate()
-//   const handleSubmit = event => {
-//     event.preventDefault()
-//     const form = event.target
-//     const email = form.email.value;
-//     const password = form.password.value;
-//     console.log(email,password)
-//     signin(email, password)
-//       .then(result => {
-//         const user = result.user
-//         form.reset() 
-//         naviget('/')
-//         console.log(user)
-//       })
-//       .catch(error => {
-//         console.error(error)
-//       })
-//   }
-//   return (
-//     <Form onSubmit={handleSubmit}>
-//     <Form.Group className="mb-3" controlId="formBasicEmail">
-//       <Form.Label>Email address</Form.Label>
-//       <Form.Control type="email" name='email' placeholder="Enter email" />
-//       <Form.Text className="text-muted">
-//         We'll never share your email with anyone else.
-//       </Form.Text>
-//     </Form.Group>
+const Login = () => {
+  const { signin } = useContext(AuthContext)
+  const naviget = useNavigate()
+  const handleSubmit = event => {
+    event.preventDefault()
+    const form = event.target
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email,password)
+    signin(email, password)
+      .then(result => {
+        const user = result.user
+        form.reset() 
+        naviget('/')
+        console.log(user)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+  return (
+    <Form onSubmit={handleSubmit}>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" name='email' placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
 
-//     <Form.Group className="mb-3" controlId="formBasicPassword">
-//       <Form.Label>Password</Form.Label>
-//       <Form.Control type="password" name="password" placeholder="Password" />
-//     </Form.Group>
-//     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-//       <Form.Check type="checkbox" label="Check me out" />
-//     </Form.Group>
-//     <Button variant="primary" type="submit">
-//       Submit
-//     </Button>
-//   </Form>
-//   );
-// };
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" name="password" placeholder="Password" />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group>
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
+  );
+};
 
-// export default Login;
+export default Login;
